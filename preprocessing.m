6ddf60b18c5a741d2374a4d72b3695e9676e055d
Nicolò <brunonico787@gmail.com>
2026-04-18 09:35:14 +0200

    change order of filters in preprocessing.m

--- preprocessing.m
+++ preprocessing.m
@@ -16,22 +16,25 @@ function output_img = preprocessing(input_data)
     img = imresize(img, [256 256]);
     img = im2double(img);
 
-    % 1. Median filter (salt-and-pepper / high-freq noise removal)
-    filtered_median = medfilt2(img, [3 3]);
-
-    % 2. Low-pass filter (Gaussian smoothing)
-    h_low = fspecial('gaussian', [3 3], 1.0);
-    filtered_low = imfilter(filtered_median, h_low, 'replicate');
-
-    % 3. High-pass filter (Laplacian sharpening)
+    
+    % High-pass filter (Laplacian sharpening)
     h_high = fspecial('laplacian', 0.2);
-    lap = imfilter(filtered_low, h_high, 'replicate');
-    filtered_high = filtered_low - lap;
-    filtered_high(filtered_high < 0) = 0;
-    filtered_high(filtered_high > 1) = 1;
+    lap = imfilter(img, h_high, 'replicate');
+    filtered_high = img - lap;
+    filtered_high = mat2gray(filtered_high);
+    
+    % Median filter (salt-and-pepper / high-freq noise removal)
+    filtered_median = medfilt2(filtered_high, [3 3]);
+    filtered_median = mat2gray(filtered_median);
+
+    % Low-pass filter (Gaussian smoothing)
+    h_low = fspecial('gaussian', [5 5], 1.5);
+    filtered_low = imfilter(filtered_median, h_low, 'replicate');
+    filtered_low = mat2gray(filtered_low);
+
 
     % The output is in [0, 1]. `skull_stripping` uses a threshold
     % of 30, which implies it expects an image in [0, 255]. We scale it up.
     % We convert to uint8 so skull_stripping works correctly with its threshold.
-    output_img = im2uint8(filtered_high);
+    output_img = im2uint8(filtered_low);
 end
